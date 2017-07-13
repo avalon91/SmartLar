c1c5b30a265aaebd4102f2557a63eef8070d215c
avalon91 <lucaspwo@gmail.com>
2017-07-13 19:33:58 -0300

    Não finalizado

---
+++ MicroPython/mqtt_rfid2.py
@@ -0,0 +1,123 @@
+#https://github.com/micropython/micropython-lib/tree/master/umqtt.simple
+#https://github.com/micropython/micropython-lib/blob/master/umqtt.simple/example_pub.py
+#https://github.com/micropython/micropython-lib/pull/91#issuecomment-239030008
+
+def tempo():
+    tp2 = utime.ticks_ms()
+    return tp2
+
+def conectar():
+    import mfrc522, machine, time, network, json, utime
+    from os import uname
+    from umqtt.simple import MQTTClient
+
+    lGre = machine.Pin(13, machine.Pin.OUT)
+    lRed = machine.Pin(12, machine.Pin.OUT)
+
+    if uname()[0] == 'WiPy':
+        rdr = mfrc522.MFRC522("GP14", "GP16", "GP15", "GP22", "GP17")
+
+    elif uname()[0] == 'esp8266':
+        rdr = mfrc522.MFRC522(0, 2, 4, 5, 14)
+
+    else:
+        raise RuntimeError("Unsupported platform")
+
+    preJson = open('config.txt').read()
+    data = json.loads(preJson)
+    config = data['campos']
+
+    SERVER = config[2]
+    TOPIC1 = b"/rfid/normal"
+    TOPIC2 = b"/rfid/teste"
+    ID = "esp"
+    USER = config[4].encode()
+    PASSWORD = config[3].encode()
+
+    def sub_cb(topic, msg):
+        #print((topic, msg))
+        msg = msg.decode("utf-8")
+        print(msg)
+        if msg == '1':
+            lGre.high()
+        if msg == '0':
+            lRed.high()
+            tp2 = utime.ticks_ms()
+
+    c = MQTTClient(ID, SERVER, user=USER, password=PASSWORD)
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(TOPIC2)
+
+    def check_rfid():
+        uid = ""
+
+        # print("")
+        # print("Place card before reader to read from address 0x08")
+        # print("")
+
+        (stat, tag_type) = rdr.request(rdr.REQIDL)
+
+        if stat == rdr.OK:
+
+            (stat, raw_uid) = rdr.anticoll()
+
+            if stat == rdr.OK:
+
+                #print("New card detected")
+                #print("  - tag type: 0x%02x" % tag_type)
+                #print("%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+
+                for i in range(0, 4):
+                    uid = uid + "%02x" % raw_uid[i]
+                uid = uid + "0"
+                #c.connect()
+                c.publish(TOPIC, b"%s" % uid)
+                #c.disconnect()
+
+    def timeHandler():
+        time = tempo()
+        if((lGre.value() == True) and (utime.ticks_diff(utime.ticks_ms(), tp2) < 5000)):
+            #print(utime.ticks_diff(utime.ticks_ms(), tp2))
+            time = tempo()
+            #lGre.low()
+        # elif(flag == True):
+        #     time2 = utime.ticks_ms()
+        #     flag = None
+        elif((lRed.value() == True) and (utime.ticks_diff(utime.ticks_ms(), tp2) >= 2000)):
+            print(utime.ticks_diff(utime.ticks_ms(), tp2))
+            lRed.low()
+        # elif(flag == False):
+        #     time2 = utime.ticks_ms()
+        #     flag = None
+
+    while True:
+        check_rfid()
+        c.check_msg()
+        timeHandler()
+        
+        # uid = ""
+
+        # # print("")
+        # # print("Place card before reader to read from address 0x08")
+        # # print("")
+
+        # (stat, tag_type) = rdr.request(rdr.REQIDL)
+
+        # if stat == rdr.OK:
+
+        #     (stat, raw_uid) = rdr.anticoll()
+
+        #     if stat == rdr.OK:
+
+        #         #print("New card detected")
+        #         #print("  - tag type: 0x%02x" % tag_type)
+        #         #print("%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+
+        #         for i in range(0, 4):
+        #             uid = uid + "%02x" % raw_uid[i]
+        #         uid = uid + "0"
+        #         #c.connect()
+        #         c.publish(TOPIC, b"%s" % uid)
+        #         #c.disconnect()
+        #         c.check_msg()
