3d531356acf8c994be565fd63779f05be53fbd1c
Lucas Oliveira <avalon91@users.noreply.github.com>
2017-12-11 19:12:57 -0300

    Funcional+rfid hardcoded
    Comunicação funcionando. Falta: servidor e LED rgb endereçável

--- MicroPython/api_rfid.py
+++ MicroPython/api_rfid.py
@@ -1,23 +1,22 @@
 tp2 = 0
 
 def conectar():
-    import mfrc522, machine, time, json, utime, urequests
+    import mfrc522, time, json, utime, urequests
     from os import uname
+    from machine import Pin
 
-    lGre = machine.Pin(13, machine.Pin.OUT)
-    lRed = machine.Pin(12, machine.Pin.OUT)
-    rele = machine.Pin(16, machine.Pin.OUT)
-    rele.low()
+    lGre = Pin(13, Pin.OUT)
+    lRed = Pin(12, Pin.OUT)
+    rele = Pin(16, Pin.OUT)
+    rele.off()
+
+    linkEntrada = 'http://lar.ect.ufrn.br:8080/api/v1/verificar-entrada/'
 
     rdr = mfrc522.MFRC522(0, 2, 4, 5, 14)
 
-    preJson = open('config.txt').read()
-    data = json.loads(preJson)
-    config = data['campos']
-
     def check_rfid():
         uid = ""
-
+        global tp2
         (stat, tag_type) = rdr.request(rdr.REQIDL)
 
         if stat == rdr.OK:
@@ -25,19 +24,39 @@ def conectar():
             if stat == rdr.OK:
                 for i in range(0, 4):
                     uid = uid + "%02x" % raw_uid[i]
+                uid = uid.upper()
                 print(uid)
+                if uid == '647A19B8' or uid == 'E696D04B' or uid == '72D0D54B' or uid == '2D30CE4B' or uid == '1DAFDF4B' or uid == 'E617D247':
+                    linkCompl = linkEntrada + uid
+                    rele.on()
+                    tp2 = utime.ticks_ms()
+                    perm = json.loads(urequests.get(linkCompl).content)
+                    print('Ola')
+                    if perm:
+                        print('Conectado e verificado')
+                else:
+                    linkCompl = linkEntrada + uid
+                    perm = json.loads(urequests.get(linkCompl).content)
+                    if perm == True:
+                        rele.on()
+                        tp2 = utime.ticks_ms()
+                        print('Entrada permitida')
+                    elif perm == False:
+                        lRed.on()
+                        tp2 = utime.ticks_ms()
+                        print('Entrada negada')
                 # uid = uid + "0"
                 # c.publish(TOPIC1, b"%s" % uid)
-                lGre.high()
+                lGre.on()
     
     def timeHandler():
         if((rele.value() == True) and (utime.ticks_diff(utime.ticks_ms(), tp2) >= 5000)):
-            lGre.low()
-            rele.low()
-            print('foi')
+            lGre.off()
+            rele.off()
+            print('Fechou')
         elif((lRed.value() == True) and (utime.ticks_diff(utime.ticks_ms(), tp2) >= 2000)):
-            lGre.low()
-            lRed.low()
+            lGre.off()
+            lRed.off()
 
     while True:
         timeHandler()
